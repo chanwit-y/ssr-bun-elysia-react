46d60485d9759bfb4d4ce24cf1386b665660d24b
cy <chanwit.yimneam@hotmail.com>
2024-03-22 14:42:56 +0700

    setup file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
 import { Elysia } from "elysia";
+import { staticPlugin } from "@elysiajs/static";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import { createElement } from "react";
+import { renderToReadableStream } from "react-dom/server";
+import App from "./react/App";
+
+await Bun.build({
+  entrypoints: ['./src/react/index.tsx'],
+  outdir: './public',
+});
+
+const app = new Elysia()
+  .use(staticPlugin())
+  .get("/", async () => {
+    const app = createElement(App);
+
+    const stream = await renderToReadableStream(app, {
+      bootstrapScripts: ["./public/index.js"],
+    });
+
+    return new Response(stream, {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
